ded67f170b5c4c01a78ac6490bfe9355f22630f1
Daniel Robles <drobles829@gmail.com>
2020-02-02 14:47:00 -0600

    Inicio del proyecto medrep-express

---
+++ src/views/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Home Page</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default HomePage;
